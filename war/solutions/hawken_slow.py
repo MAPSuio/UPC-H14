fa920506679ae3375b3d074820496c341b54788f
hawken93 <hawken@thehawken.org>
2014-11-04 20:09:35 +0100

    war: tested lists in python

--- war/solutions/hawken_slow.py
+++ war/solutions/hawken_slow.py
@@ -1,17 +1,16 @@
 # @EXPECTED_RESULTS@: CORRECT
 
-from collections import deque
 from sys import stdin
 
 # Read
 n = int(stdin.next().strip())
 m = int(stdin.next().strip())
 
-p1stack = deque()
-p2stack = deque()
+p1stack = []
+p2stack = []
 
-p1bet = deque()
-p2bet = deque()
+p1bet = []
+p2bet = []
 
 p1card = None
 p2card = None
@@ -22,10 +21,13 @@ if n:
 if m:
     line = stdin.next()
     [p2stack.append(int(x)) for x in line.strip().split()]
+
 iter = 0
 while len(p1stack) and len(p2stack):
-    p1card = p1stack.popleft()
-    p2card = p2stack.popleft()
+    p1card = p1stack[0]
+    p1stack = p1stack[1:]
+    p2card = p2stack[0]
+    p2stack = p2stack[1:]
     if p1card == p2card:
         # Put their current cards as bet
         p1bet.append(p1card)
@@ -33,32 +35,44 @@ while len(p1stack) and len(p2stack):
         # Put three new as bet
         for i in xrange(0,3):
             if len(p1stack):
-                p1bet.append(p1stack.popleft())
+                v = p1stack[0]
+                p1stack = p1stack[1:]
+                p1bet.append(v)
             if len(p2stack):
-                p2bet.append(p2stack.popleft())
+                v = p2stack[0]
+                p2stack = p2stack[1:]
+                p2bet.append(v)
     elif p1card > p2card:
         while len(p1bet):
-            p1stack.append(p1bet.popleft())
+            v = p1bet[0]
+            p1bet = p1bet[1:]
+            p1stack.append(v)
 
         p1stack.append(p1card)
 
         while len(p2bet):
-            p1stack.append(p2bet.popleft())
+            v = p2bet[0]
+            p2bet = p2bet[1:]
+            p1stack.append(v)
 
         p1stack.append(p2card)
     else:
         while len(p2bet):
-            p2stack.append(p2bet.popleft())
+            v = p2bet[0]
+            p2bet = p2bet[1:]
+            p2stack.append(v)
 
         p2stack.append(p2card)
 
         while len(p1bet):
-            p2stack.append(p1bet.popleft())
+            v = p1bet[0]
+            p1bet = p1bet[1:]
+            p2stack.append(v)
 
         p2stack.append(p1card)
     iter = iter + 1
 
-if (not len(p1stack) and not len (p2stack)) or iter >= 100000:
+if (not len(p1stack) and not len(p2stack)) or iter >= 100000:
     print "draw"
 elif not len(p1stack):
     print "PLAYER2"
