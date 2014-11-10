8a6ef02378da965fa696621b59553eab79c64205
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2014-11-10 01:06:14 +0100

    socketzz: c solution is done

--- socketzz/solutions/hennilu.c
+++ socketzz/solutions/hennilu.c
@@ -7,42 +7,44 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-static int my_udp_socket = 0;
+#include "log.h"
+
+static int udp_socket = 0;
 static int BUF_SIZ = 256;
+static int PORT = 3950;
 
-/**
- * Called at the start of the program. Initializes data structures
- * like an operating system would do at boot time.
- *
- * local_port is the port that for the local UDP socket that is used for all 
- * communication.
- *
- * This function initializes a socket for the server.
- */
-void net_init(int local_port) 
+void net_init() 
 {
-	struct sockaddr_in addr;
-	int err;
+	udp_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-	my_udp_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-	if(my_udp_socket < 0) {
+	if(udp_socket < 0) {
 		_ERR("Failed to create local UDP socket");
+        perror("socket()");
 		exit(-1);
 	}
+}
+
+void net_send()
+{
+    struct sockaddr_in addr;
+    int retval;
+    char msg[256];
+    int msg_size;
 
 	memset(&addr, 0, sizeof(struct sockaddr_in));
 	addr.sin_family      = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port        = htons(local_port);
+	addr.sin_port        = htons(PORT);
 
-	/* Bind to the socket */
-	err = bind(my_udp_socket, (struct sockaddr*)&addr, sizeof(struct sockaddr_in));
+    msg_size = create_message(msg);
 
-	if(err < 0) {
-		_ERR("Failed to bind local UDP socket to port %d", local_port);
-		exit(-1);
-	}
+    retval = sendto(udp_socket, msg, msg_size, 0, (struct sockaddr *) &addr, sizeof(addr));
+
+    if (retval == -1) {
+        _ERR("Sendto failed: ");
+        perror("sendto()");
+        exit(-1);
+    }
 }
 
 void net_recv()
@@ -50,29 +52,66 @@ void net_recv()
 	struct sockaddr_in src_addr;
 	socklen_t addrlen = sizeof(struct sockaddr_in);
 
-	char buffer[BUFFER_SIZ];
+	char buffer[BUF_SIZ];
 	memset(buffer, 0, (sizeof(char) * BUF_SIZ));
 
 	ssize_t recvd_bytes = 0;
 
-	recvd_bytes = recvfrom(my_udp_socket, &buffer, (sizeof(char) * BUF_SIZ), 0, 
+	recvd_bytes = recvfrom(udp_socket, &buffer, (sizeof(char) * BUF_SIZ), 0, 
                             (struct sockaddr*) &src_addr, &addrlen);
+
 
 	if (recvd_bytes < 0) {
 		_ERR("Failed to recv data:");
 		perror("recvfrom()");
-		return;
+        exit(-1);
 	} 
 
-    buffer[recvd_bytes - 1] = '\0'; // Null terminate for prettyness
+    printf("%s\n", &buffer[sizeof(uint8_t) * 4]);
+}
 
-    printf("Message received: %s\n", buffer);
+int create_message(char* msg)
+{
+    /* Data types */
+    uint8_t length;
+    uint8_t type;
+    uint8_t input1;
+    uint8_t input2;
+
+    /* Message size */
+    int size = sizeof(uint8_t) * 4;
+
+    /* Indexes */
+    int length_index;
+    int type_index;
+    int input1_index;
+    int input2_index;
+
+    /* Data assignation */
+    length = size;
+    type   = 0x02;
+    input1 = 20;
+    input2 = 10;
+
+    /* Index declaration */
+    length_index = 0;
+    type_index   = 1;
+    input1_index = 2;
+    input2_index = 3;
+
+    /* Inserting into message */
+    msg[length_index] = length;
+    msg[type_index]   = type;
+    msg[input1_index] = input1;
+    msg[input2_index] = input2;
+
+    return size;
 }
 
 int main(int argc, char** argv)
 {
-    int local_port = 3950;
-
-    net_init(local_port);
+    net_init();
+    net_send();
+    net_recv();
     return 0;
 }
