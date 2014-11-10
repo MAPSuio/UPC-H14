71197ad561a4253c203e8c910fd434ff240527d6
Joakim Lindquister <joakiml@gmail.com>
2014-11-10 11:05:44 +0100

    Use same class name as file name

--- birthdays/solutions/joakimjl.java
+++ birthdays/solutions/joakimjl.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.Scanner;
 
-public class Main{
+public class joakimjl{
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
