daaf46a91cad3f4fe69724cd5489879bfbf133af
hawken93 <hawken@thehawken.org>
2014-11-10 21:45:06 +0100

    prize: updated hawken.c

--- prize/solutions/hawken.c
+++ prize/solutions/hawken.c
@@ -2,38 +2,28 @@
 # @EXPECTED_RESULTS@: CORRECT
 */
 #include <stdio.h>
-#include <malloc.h>
 #include <string.h>
 
 int main() {
-	int n, i, j, place = 1;
-	double iphone = -1, *p, tmp;
+	int n, place = 1;
+	double iphone = 5790.000000, tmp;
 	char name[51];
 
 	scanf("%d", &n);
-	p = malloc(sizeof(iphone) * (n-1));
-	j = 0;
+	n--;
 
 	// Read and find the iphone
-	for(i=0;i<n-1;i++) {
+	for(;n;n--) {
 		scanf("%s", name);
 		scanf("%lf", &tmp);
-		if(!strcmp(name, "iPhone"))
-			iphone = tmp;
-		else
-			p[j++] = tmp;
-	}
-
-	// Count the number of prices bigger than iphone
-	for(i=0;i<j;i++)
-		if(p[i] > iphone)
+		if(tmp > iphone)
 			place++;
+	}
 
 	if(place == 1)
 		printf("STEAL THE PHONE\n");
 	else
 		printf("%d\n", place);
 
-	free(p);
 	return 0;
 }
