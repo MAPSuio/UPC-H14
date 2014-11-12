9ba9d418792ee7f0e471d98cf957292d640ebc74
hawken93 <hawken@thehawken.org>
2014-11-12 13:53:21 +0100

    accepting festival

--- festival/solution.md
+++ festival/solution.md
@@ -1,2 +1,10 @@
-# <problem title here>
-<short explanation of how to solve problem here>
+# Festival area
+
+Implement http://en.wikipedia.org/wiki/Shoelace_formula.
+
+Shoelace formula pseudocode:
+* Points: [a, b, c, d]
+* Pairs: \[a, b\] \[b, c\] \[c, d\] \[d,a\]
+* for all pairs as a, b
+ * sum += a.x*b.y - b.x*a.y
+* sum = abs(sum)/2
--- festival/solutions/hawken.c
+++ festival/solutions/hawken.c
@@ -1,13 +1,6 @@
 /*
 @EXPECTED_RESULTS@: CORRECT
- * Idea: Calculate the polygon by triangles, until only two points remain, then return the absolute value
- *
- * points: a b c d e f g
- * ......> a c d e f g
- * ......> a d e f g
- * ......> a e f g
- * ......> a f g
- * ......> solution
+ * Do the shoelace formula
  *
  * Additionally, do the edge sum to find out which direction the graph is going.
  * This means it will ignore triangles going outside
@@ -15,57 +8,46 @@
 
 #include <stdio.h>
 
+#define fl long double
+
 typedef struct point_t point;
 struct point_t {
-	double x, y;
+	fl x, y;
 };
 
-// http://www.mathopenref.com/coordtrianglearea.html
-inline double trianglesum(point a, point b, point c) {
-	// absolute value of:
-	// ax(by-cy) + Bx(cy-ay) + Cx(ay-by) / 2
-	double ai = b.y - c.y;
-	double bi = c.y - a.y;
-	double ci = a.y - b.y;
-	return ((a.x*ai) + (b.x*bi) + (c.x * ci)) / (double)2.0;
-}
-
-// http://stackoverflow.com/questions/1165647/how-to-determine-if-a-list-of-polygon-points-are-in-clockwise-order
-inline double edgesum(point a, point b) {
-	double x = b.x - a.x;
-	double y = b.y + a.y;
-	return x*y;
+// http://en.wikipedia.org/wiki/Shoelace_formula
+inline fl edgesum(point a, point b) {
+	fl x = a.x*b.y;
+	fl y = b.x*a.y;
+	return x - y;
 }
 
 int main() {
 	int n;
-	double area = 0, direction = 0;
+	fl area = 0;
 	point a, b, c;
 
 	scanf("%d", &n);
-	scanf("%lf %lf", &a.x, &a.y);
-	scanf("%lf %lf", &b.x, &b.y);
+	scanf("%Lf %Lf", &a.x, &a.y);
+	scanf("%Lf %Lf", &b.x, &b.y);
 
-	direction += edgesum(a,b);
+	area += edgesum(a,b);
 	n -= 2;
 	while(n > 0) {
 		// read into c
-		scanf("%lf %lf", &c.x, &c.y);
-
-		// calculate into tmp
-		area += trianglesum(a,b,c);
+		scanf("%Lf %Lf", &c.x, &c.y);
 
 		// calculate the edge
-		direction += edgesum(b,c);
+		area += edgesum(b,c);
 
 		// move c to b
 		b.x = c.x;
 		b.y = c.y;
 		n--;
 	}
-	direction += edgesum(b, a);
+	area += edgesum(b, a);
 
-	if(direction > 0) area = -area;
-	printf("%lf\n", area);
+	area = ((area < 0) ? -area : area) / (fl)2;
+	printf("%.8Lf\n", area);
 	return 0;
 }
